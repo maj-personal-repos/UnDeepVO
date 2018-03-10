37ed4ec44ae194ee0d9a28f0cdfaefc06e5aff6c
Miguel Alonso Jr <drmiguelalonsojr@gmail.com>
2018-03-10 16:05:32 -0500

    updated image_loader

--- image_loader.py
+++ image_loader.py
@@ -3,57 +3,56 @@ import numpy as np
 
 
 def get_stereo_image_generators(train_folder, test_folder, img_rows=128, img_cols=512, batch_size=16, shuffle=True):
-    train_imagegen = ImageDataGenerator(rescale=1.0 / 255.0,
-                                        rotation_range=5,
-                                        shear_range=0.01,
-                                        zoom_range=0.01,
-                                        height_shift_range=0.01,
-                                        width_shift_range=0.01)
+    train_image_gen = ImageDataGenerator(rescale=1.0 / 255.0,
+                                         rotation_range=5,
+                                         shear_range=0.01,
+                                         zoom_range=0.01,
+                                         height_shift_range=0.01,
+                                         width_shift_range=0.01)
 
-    test_imagegen = ImageDataGenerator(rescale=1.0 / 255.0)
+    test_image_gen = ImageDataGenerator(rescale=1.0 / 255.0)
 
-    train_generator_left = train_imagegen.flow_from_directory(train_folder,
-                                                              target_size=(img_rows, img_cols),
-                                                              batch_size=batch_size,
-                                                              seed=10,
-                                                              shuffle=shuffle,
-                                                              classes=['left'],
-                                                              class_mode=None,
-                                                              follow_links=True)
-
-    train_generator_right = train_imagegen.flow_from_directory(train_folder,
+    train_generator_left = train_image_gen.flow_from_directory(train_folder,
                                                                target_size=(img_rows, img_cols),
                                                                batch_size=batch_size,
                                                                seed=10,
                                                                shuffle=shuffle,
-                                                               classes=['right'],
+                                                               classes=['left'],
                                                                class_mode=None,
                                                                follow_links=True)
 
-    test_generator_left = test_imagegen.flow_from_directory(test_folder,
-                                                            target_size=(img_rows, img_cols),
-                                                            batch_size=batch_size,
-                                                            seed=10,
-                                                            shuffle=shuffle,
-                                                            classes=['left'],
-                                                            class_mode=None,
-                                                            follow_links=True)
+    train_generator_right = train_image_gen.flow_from_directory(train_folder,
+                                                                target_size=(img_rows, img_cols),
+                                                                batch_size=batch_size,
+                                                                seed=10,
+                                                                shuffle=shuffle,
+                                                                classes=['right'],
+                                                                class_mode=None,
+                                                                follow_links=True)
 
-    test_generator_right = test_imagegen.flow_from_directory(test_folder,
+    test_generator_left = test_image_gen.flow_from_directory(test_folder,
                                                              target_size=(img_rows, img_cols),
                                                              batch_size=batch_size,
                                                              seed=10,
                                                              shuffle=shuffle,
-                                                             classes=['right'],
+                                                             classes=['left'],
                                                              class_mode=None,
                                                              follow_links=True)
+
+    test_generator_right = test_image_gen.flow_from_directory(test_folder,
+                                                              target_size=(img_rows, img_cols),
+                                                              batch_size=batch_size,
+                                                              seed=10,
+                                                              shuffle=shuffle,
+                                                              classes=['right'],
+                                                              class_mode=None,
+                                                              follow_links=True)
 
     def train_generator_func():
         while True:
             left_image = train_generator_left.next()
-            right_image = train_generator_right.next()
 
-            # output = np.concatenate((left_image, right_image), axis=2)
+            right_image = train_generator_right.next()
 
             yield [left_image, right_image], [left_image,
                                               right_image,
@@ -63,9 +62,8 @@ def get_stereo_image_generators(train_folder, test_folder, img_rows=128, img_col
     def test_generator_func():
         while True:
             left_image = test_generator_left.next()
-            right_image = test_generator_right.next()
 
-            # output = np.concatenate((left_image, right_image), axis=2)
+            right_image = test_generator_right.next()
 
             yield [left_image, right_image], [left_image,
                                               right_image,
@@ -73,6 +71,11 @@ def get_stereo_image_generators(train_folder, test_folder, img_rows=128, img_col
                                               np.zeros(shape=right_image.shape)]
 
     train_generator = train_generator_func()
+
     test_generator = test_generator_func()
 
-    return train_generator, test_generator
+    train_length = len(train_generator_left.filenames)
+
+    test_length = len(test_generator_left.filenames)
+
+    return train_generator, test_generator, train_length, test_length
